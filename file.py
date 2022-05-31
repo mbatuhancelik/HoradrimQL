cc1b69370d1d53bb6ba1a632c6e7160e18503321
mbatuhancelik <mbatuhancelik>
2022-05-31 09:53:55 +0300

    deleting records by loading the whole file

--- file.py
+++ file.py
@@ -1,26 +1,27 @@
 import os.path
 from page import Page
 
-
-
 maxPages = 10
 headerLen = 1000
+
+
 class PageMeta:
     def __init__(self, table, type, len, full):
         self.table = table
         self.type = type
         self.len = len
         self.full = full
-        
+
+
 class File:
     def __init__(self, filename):
         self.fileName = filename
-       
+
         self.exists = False
-        self.pages = {} 
+        self.pages = {}
         self.pageIds = []
 
-        if(os.path.exists(filename)):
+        if os.path.exists(filename):
             self.exists = True
             self.loadFromFile()
 
@@ -34,30 +35,35 @@ class File:
             page = pages[id]
             metadata = page.split("|")
             id = int(metadata[0])
-            self.pages[id] = PageMeta(metadata[1], metadata[2], int(metadata[3]), bool(int(metadata[4])))
+            self.pages[id] = PageMeta(
+                metadata[1], metadata[2], int(metadata[3]), bool(int(metadata[4]))
+            )
             self.pageIds.append(id)
-        
+
     def availableId(self):
-        if(len(self.pages.keys()) == maxPages ):
+        if len(self.pages.keys()) == maxPages:
             raise Exception("File is full")
-        
+
         max = len(self.pages)
 
         for i in self.pageIds:
             if i > max:
                 max = i + 1
 
-        return max 
+        return max
+
     def getIds(self):
         ids = []
         for i in range(maxPages):
-            if i in self.pages.keys(): 
+            if i in self.pages.keys():
                 ids.append(i)
-        
+
         return ids
+
     """
     creates header, stringifies pages and writes them down
     """
+
     def initializeFile(self):
         f = open(self.fileName, "w")
         self.exists = True
@@ -69,21 +75,18 @@ class File:
         id = self.availableId()
         str = Page.stringify()
         self.currentPage = Page
-        self.pages[id] = PageMeta(Page.tableName, Page.type, len(str) , Page.isFull())
+        self.pages[id] = PageMeta(Page.tableName, Page.type, len(str), Page.isFull())
         self.pageIds.append(id)
-        
+
         self.updateHeader()
-        
-        
+
         f = open(self.fileName, "a")
         f.write(str)
         f.close()
-        
 
         f.close()
 
-
-    def updateHeader (self):
+    def updateHeader(self):
 
         self.header = ""
         for pageId in self.pageIds:
@@ -103,18 +106,18 @@ class File:
         for i in range(headerLen - len(self.header)):
             self.header += " "
         self.header += "\n"
-        
+
         if not self.exists:
             self.initializeFile()
         f = open(self.fileName, "r+")
         f.seek(0)
         f.write(self.header)
         f.close()
-        
+
     def writePage(self):
         pass
-
-    def loadPage(self, pageId):
+    
+    def loadPageString(self, pageId):
         f = open(self.fileName, "r")
         chars = headerLen + 1
         ## this should take relative position of the page in the ids list
@@ -124,19 +127,49 @@ class File:
         f.seek(chars)
         s = f.read(self.pages[pageId].len)
         f.close()
-
-        p = Page("","","")
+        return s
+    def loadPage(self, pageId):
+        
+        s = self.loadPageString(pageId)
+        p = Page("", "", "")
         p.loadFromString(s)
 
         return p
+    def deletePage(self, pageId):
+        if pageId not in self.pageIds:
+            raise Exception("Deleting nonexisting page")
+        pageStrings = {}
+
+        for i in self.pageIds:
+            if not i == pageId:
+                pageStrings[i] = self.loadPageString(i)
+        
+        self.initializeFile()
+        self.pageIds.remove(pageId)
+        del self.pages[pageId]
+        self.updateHeader()
+
+        f = open(self.fileName, "a")
+        for i in self.pageIds:
+            f.write(pageStrings[i])
+        f.close()
+
 
 f1 = File("halo.txt")
-p = Page("table1", "record" , [int, int, str])
-p.insert([0,0,"halo"])
-p.insert([1,0,"halo"])
+p = Page("table1", "record", [int, int, str])
+p.insert([0, 0, "halo"])
+p.insert([1, 0, "halo"])
+f1.addPage(p)
+p = Page("table2", "record", [int, int, str])
+p.insert([0, 0, "haloo"])
+p.insert([1, 0, "haloo"])
+f1.addPage(p)
+p = Page("table3", "record", [int, int, str])
+p.insert([0, 0, "halooo"])
+p.insert([1, 0, "halooo"])
+f1.addPage(p)
 # f1.addPage(p)
-# f1.addPage(p)
-p = f1.loadPage(7)
+f1.deletePage(1)
 # f1.createPage("table2", "record" , [int, int, str, str])
 # p = f1.getPage(0)
 # p.delete(0)
